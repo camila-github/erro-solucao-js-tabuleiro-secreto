cafaea5d2645ef8cb3464a30a734f0c7fdcf1ba1
Camila Augusta dos Santos <camilaaugustasantos@yahoo.com.br>
2021-01-01 18:07:33 -0300

    TabuleiroSecreto

---
+++ README.md
@@ -0,0 +1,1 @@
+"# erro-solucao-tabuleiro-secreto" 
---
+++ js-tabuleiro-secreto.js
@@ -0,0 +1,15 @@
+// SOLUCAO 1 - Usado função anonima
+(function hashMagico(numEntrada) {
+    while (numEntrada--) {
+        entradas = [];
+        resultado = 0;
+        numCasos = parseInt(gets());
+
+        while (numCasos--) entradas = [...entradas, gets()];
+
+        for (let [index, linha] of entradas.entries()) {
+            resultado += linha.match(/[A-Z]/g).reduce((valorAcc, curr, strIndex) => valorAcc + (parseInt(curr, 36) - 10) + index + strIndex, 0);
+        }
+        console.log(resultado);
+    }
+})(gets());
